7e50da86d8309402ba37fa54953a3578b0e8a2c9
YoungSan14 <suarezsan14@gmail.com>
2023-03-10 19:44:57 -0300

    se agrega axios para una peticion en Detail

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
@@ -9,42 +10,88 @@ const DivStyled = styled.div`
     height: 100vh;
     width: 100vw;
     display: flex;
-    justify-content: center;
+    justify-content: space-evenly;
     align-items: center;
     position: relative;
+    font-family: 'Press Start 2P';
     & img{
         border-radius: 100%;
         object-fit: contain;
         border: 5px solid #BBEF55;
     }
+    .info{
+        display: flex;
+        flex-direction: column;
+        color: white;
+    }
+    .male{
+    color: #74C2E7;
+    }
+    .female{
+        color: #AB73A1;
+    }
+    .genderless{
+        color: #6E8774;
+    }
+    .unknown{
+        color: #861D21;
+    }
 `;
 
 export default function Detail(){
-    const [ chacter , setChacter ] = useState();
+    const [ chacter , setChacter ] = useState({});
     const { idChacter } = useParams();
 
+    // useEffect(() => {
+    //     fetch(`${URL}/character/${idChacter}?key=${KEY}`)
+    //     .then((res) => res.json())
+    //     .then((character) => {
+    //         if(character.name){
+    //             setChacter( character );
+    //         }else{
+    //             alert('No existe ese personaje');
+    //         }
+    //     })
+    //     .catch((error) => console.log(error));
+    //     return setChacter({});
+    // }, [idChacter]);
     useEffect(() => {
-        fetch(`${URL}/character/${idChacter}?key=${KEY}`)
-        .then((res) => res.json())
-        .then((character) => {
-            if(character.name){
-                setChacter( character );
-            }else{
-                alert('No existe ese personaje');
-            }
-        })
-        .catch((error) => console.log(error));
-        return setChacter({});
+        axios(`${URL}/character/${idChacter}?key=${KEY}`)
+        .then((res) => setChacter(res.data))
     }, [idChacter]);
+
+    const genderStyle = (gender) => {
+        if(gender === 'Male') return 'male'
+        if(gender === 'Female') return 'female' 
+        if(gender === 'Genderless') return 'genderless' 
+        if(gender === 'unknown') return 'unknown'
+    }
 
     return (
         <DivStyled>
-            {
-                console.log(chacter?.name)
+            {           
+            (chacter.name) ? (
+                <>
+                    <img src={chacter.image} alt={chacter.name} />
+                    <div className='info'>
+                        <h1 style={{color: '#BBEF55'}}>{chacter.name}</h1>
+                        Genero:<h2 className={genderStyle(chacter.gender)}>{chacter.gender}</h2>  
+                        Especie:<h2>{chacter.species}</h2>
+                        Origen:<h2>{chacter.origin?.name}</h2>
+                    </div>
+                </>
+            )
+            : (
+                <>
+                    <h1 style={{color: 'white'}}>Cargando...</h1>
+                </>
+            )
             }
-            {/* <img src={chacter.image} alt={chacter.name} /> */}
-            <img src={chacter?.image} alt={chacter?.name} />
-            <h1>{chacter?.name}</h1>
         </DivStyled>
     )
 }
+
+
+
+    // {/* console.log(chacter?.name) */}
+
